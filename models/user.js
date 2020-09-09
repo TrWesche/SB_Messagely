e67b8a04f54ac6e2a0b117bb22c1488d154a6bc3
Trenton Wesche <TrWesche@live.com>
2020-09-08 23:28:43 -0500

    Step 2 - Minor change to date insert

--- models/user.js
+++ models/user.js
@@ -13,13 +13,12 @@ class User {
    */
 
   static async register({username, password, first_name, last_name, phone}) {
-    const join_at = new Date();
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
     const result = await db.query(`
       INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
-      VALUES ($1, $2, $3, $4, $5, $6, current_timestamp)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
       RETURNING username, password, first_name, last_name, phone`,
-      [username, hashedPassword, first_name, last_name, phone, join_at]);
+      [username, hashedPassword, first_name, last_name, phone]);
     return result.rows[0]
   }
 
